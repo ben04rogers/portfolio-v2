8cf61944dd3146e26a073222bbe14fcbe976a773
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-09-28 21:29:15 +1000

    Update intro section styles

--- components/intro.tsx
+++ components/intro.tsx
@@ -71,7 +71,7 @@ export default function Intro() {
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
