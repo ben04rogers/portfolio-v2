2d465875f68e4d6feacd2e6f0e3180444de12cea
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-09-28 18:01:24 +1000

    Debugging

--- components/header.tsx
+++ components/header.tsx
@@ -30,9 +30,9 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-300 dark:hover:text-gray-300",
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-950 dark:text-white":
                       activeSection === link.name,
                   }
                 )}
