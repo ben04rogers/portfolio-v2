881edc10ed739d2a93264f562fe323555d91a192
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-10-04 20:47:28 +1000

    Update data

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,9 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { LuGraduationCap } from "react-icons/lu";
 import cryptoCrowdImg from "@/public/cryptocrowd2.png";
 import ozdevsImg from "@/public/ozdevs.png";
 import assetTradingImg from "@/public/asset-trading.jpg";
-import euphorusImg from "@/public/euphorus.png";
+import euphorusImg from "@/public/euphorus-two.png";
 import euphorusBackendImg from "@/public/euphorus-backend.png";
 import techprowlImg from "@/public/techprowl2.png";
 import taskManagerImg from "@/public/task-manager.png";
